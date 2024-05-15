9fae5faa36aca938a3f0f116e9fe4d69f94319a3
Erick Santos <erick.sousa@hotmail.com.br>
2024-05-15 15:27:42 -0300

    ✨ add counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(GetMaterialApp(
+    home: MyWidget(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class AppController extends GetxController {
+  var counter = 0.obs;
+  void add() => counter.value++;
+  void subtract() => counter.value--;
+}
+
+class MyWidget extends StatelessWidget {
+  final AppController controller = Get.put(AppController());
+  MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GetX Counter App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('O valor do contador é :',
+                style: TextStyle(fontSize: 25.0)),
+            Obx(() => Text(
+                  '${controller.counter.value}',
+                  style: const TextStyle(
+                    fontSize: 40.0,
+                  ),
+                )),
+            const SizedBox(
+              height: 15.0,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Get.to(SecondPage());
+                },
+                child: const Text('Vá para a segunda tela')),
+          ],
         ),
       ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: controller.add,
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: controller.subtract,
+            child: const Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  final AppController controller = Get.find();
+  SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Segunda Tela'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'O valor no controller é: ',
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
+            ),
+            Obx(() => Text('${controller.counter.value}',
+                style: const TextStyle(
+                  fontSize: 40.0,
+                )))
+          ],
+        ),
+      ),
+      floatingActionButton:
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+        FloatingActionButton(
+          heroTag: null,
+          onPressed: controller.add,
+          child: const Icon(Icons.add),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        FloatingActionButton(
+          heroTag: null,
+          onPressed: controller.subtract,
+          child: const Icon(Icons.remove),
+        )
+      ]),
     );
   }
 }
